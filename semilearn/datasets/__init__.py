0328147fd15ae71e0140b44f1612a4849537f4c3
nukezil <lizekun@smail.nju.edu.cn>
2023-09-12 11:23:13 +0800

    Small fixs

--- semilearn/datasets/__init__.py
+++ semilearn/datasets/__init__.py
@@ -1,5 +1,4 @@
 from semilearn.datasets.utils import split_ssl_data
-from semilearn.datasets.cv_datasets import get_cifar, get_imagenet, get_svhn
 from semilearn.datasets.cv_datasets import get_cifar_openset, get_imagenet30, \
     svhn_as_ood, lsun_as_ood, gaussian_as_ood, uniform_as_ood
 
