5f86034fb6c123ce554bbd9a1de722d06a086dee
nukezil <lizekun@smail.nju.edu.cn>
2023-12-11 13:14:59 +0800

    Small fix

--- semilearn/datasets/cv_datasets/__init__.py
+++ semilearn/datasets/cv_datasets/__init__.py
@@ -1,3 +1,3 @@
-from .cifar import get_cifar, get_cifar_openset
+from .cifar import get_cifar_openset
 from .imagenet30 import get_imagenet30
 from .ood import svhn_as_ood, lsun_as_ood, gaussian_as_ood, uniform_as_ood
